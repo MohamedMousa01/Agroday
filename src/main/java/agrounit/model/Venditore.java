5f3c855f3de65be7e00a2653eee9257272e55b3f
Mohamed Mousa <moimousa03@gmail.com>
2026-01-05 02:57:39 +0100

    added Venditore

---
+++ src/main/java/agrounit/model/Venditore.java
@@ -0,0 +1,9 @@
+package main.java.agrounit.model;
+
+public class Venditore extends Utente{
+
+    public Venditore(String idUtente, String nome, String cognome, String username, String email, String password) {
+        super(nome, cognome, username, email, password, idUtente);
+
+    }
+}
